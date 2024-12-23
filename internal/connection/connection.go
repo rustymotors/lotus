84c688862f09ee438d63f3ed5e70ff8ef5131b88
Molly Draven <drazi@duck.com>
2024-12-23 11:19:34 -0500

    feat: add data length validation in RawPacket.UnmarshalBinary

--- internal/connection/connection.go
+++ internal/connection/connection.go
@@ -31,6 +31,11 @@ func (p *RawPacket) UnmarshalBinary(data []byte) error {
 	p.Header = uint16(data[0])<<8 | uint16(data[1])
 	p.Length = uint16(data[2])<<8 | uint16(data[3])
 	p.Data = data[4:]
+
+	if len(p.Data) != int(p.Length) {
+		return fmt.Errorf("data length mismatch: expected %d, got %d", p.Length, len(p.Data))
+	}
+
 	return nil
 }
 
